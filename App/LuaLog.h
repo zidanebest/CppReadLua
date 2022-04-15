c494c773e3d01396b4cff8aa8f0aabc1222e7e58
Stereo <961461851@qq.com>
2022-04-15 23:49:04 +0800

    add gitignore & readme

--- App/LuaLog.h
+++ App/LuaLog.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LUA_ERROR(...)\
-printf("ERROR FILE:%s,  LINE:%2d\n\%s\n",__FILE__,__LINE__,__FUNCSIG__);\
+printf("ERROR FILE:%s,  LINE:%2d\n %s\n",__FILE__,__LINE__,__FUNCSIG__);\
 printf(__VA_ARGS__)\
     
 #define LUA_ASSERT(expr,...)\
---
+++ README.md
@@ -0,0 +1,6 @@
+# LuaTableToCPP
+提供访问Lua Table的API
+
+- 使用方式
+1.将表注册到Require.lua中
+2.在App.cpp中有API使用案例
